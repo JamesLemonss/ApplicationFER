e65d1abaa6f65f1242f579a1f1b1a9e62aa5b6e7
James Latanna <jamesgetgit@gmail.com>
2025-06-08 13:11:38 +0900

    commit

--- app.py
+++ app.py
@@ -251,4 +251,6 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
